d08f70def11cc583fbb163007f5bed4b17c7a92a
zhangmingming <15801572558@126.com>
2017-03-26 16:26:56 +0800

    test3
    if else 作业练习

--- 201611680672.py
+++ 201611680672.py
@@ -1,52 +1,91 @@
 
 # coding: utf-8
 
-# - 练习2：由用户指定整数个数，并由用户输入多个整数，并求和的代码
+# - 练习 1：写程序，可由键盘读入用户姓名例如Mr. right，让用户输入出生的月份与日期，判断用户星座，假设用户是金牛座，则输出，Mr. right，你是非常有性格的金牛座！。
 
-# In[7]:
+# In[1]:
 
-n=input('请输入将计算的整数个数，以回车结束')
-n=int(n)
-i=1
-sum=0
+name=input('what is your name ?')
+print('when is you birthday ?example:5月21输入为以下各式：521')
 
-while i<=n:
-    y=input('请输入一个需计算的数字')
-    y=int(y)
-    sum+=y
-    i+=1
+birthday=int(input())
+if 421<=birthday<=521 :
+        print('Mr.',name,'你是非常有个性的金牛座!')
+
+
+# - 练习 2：写程序，可由键盘读入两个整数m与n(n不等于0)，询问用户意图，如果要求和则计算从m到n的和输出，如果要乘积则计算从m到n的积并输出，如果要求余数则计算m除以n的余数的值并输出，否则则计算m整除n的值并输出。
+
+# In[2]:
+
+m=int(input('please enter an integer'))
+n=int(input('please enter an integer'))
+
+print('请输入您要执行的操作：+、*、%、else')
+operation=input()
+
+if operation=='+':
+    print(m+n)
+elif operation=='*':
+    print(m*n)
+elif  operation=='%':
+    print(m%n)
+else :
+    print(n//m)
     
-print('这几个数字的和为：')
-print(sum)
 
 
-# - 练习3：用户可以输入任意多个数字，直到用户不想输入为止
+# - 练习 3：写程序，能够根据北京雾霾PM2.5数值给出对应的防护建议。如当PM2.5数值大于500，则应该打开空气净化器，戴防雾霾口罩等
+
+# In[3]:
+
+value=int(input('请输入今天北京PM2.5的值'))
+
+if value>500:
+    print('建议打开空气净化器，出门戴口罩')
+
+
+# - 练习 4：英文单词单数转复数，要求输入一个英文动词（单数形式），能够得到其复数形式，或给出单数转复数形式的建议（提示，some_string.endswith(some_letter)函数可以判断某字符串结尾字符，可尝试运行：'myname'.endswith('me')，'liupengyuan'.endswith('n')`）。
+
+# In[6]:
+
+word=input('please enter a word ')
+
+if word.endswith('s') or word.endswith('x') or word.endswith('ch') or word.endswith('sh') or word.endswith('o') :
+    print(word,'es',sep='')
+else :
+    print(word,'s',sep='')       # 此题不全面，若为一辅音字母加Y用现学方法怎解决？
+
+
+# - 尝试性练习：写程序，能够在屏幕上显示空行。
 
 # In[8]:
 
-print('请输入任意数字，直道您不想输入为止，请输入f并回车结束')
-
-while True :
-    i=input()
-    if i=='f' :    #atention:不可以敲成：i==f
-        break
+print('name')
+print(sep='\n')
+print('name')
 
 
-# - 练习4：用户可以输入任意多个数字，直到输入的所有数字和比当前输入的数字小，且输入的所有的数字的积比当前输入的数字的平方小
+# In[10]:
 
-# In[5]:
+n=int(input('请输入将输入的数字总个数'))
+max_num=int(input('请输入一个整数'))
+secmax_num=int(input('请输入一个整数'))
+i=2
 
-print('请输入任意数字')
-sum=0
-total=1
-
-while True :
-    i=input()
-    i=int(i)
-    if i>sum and i**2 >total :
-        break
-    sum+=i
-    total*=i
-   
-    
+while i<n :
+    if max_num<secmax_num :
+        temp1=max_num
+        max_num=secmax_num
+        secmax_num=temp1
+    num=int(input('请输入一个整数'))
+    if num>max_num :
+        temp2=max_num
+        max_num=num
+        secmax_num=temp2
+    elif num>secmax_num :
+        secmax_num=num
+    i+=1
+print('The max nummber is :',max_num)
+print('The second max number is',secmax_num)
+        
 
